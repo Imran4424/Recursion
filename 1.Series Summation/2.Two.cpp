843cfd4599b32c089f20ecd3e05760764a0fcd70
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-03 23:24:02 +0600

    series summation

--- 1.Series Summation/2.Two.cpp
+++ 1.Series Summation/2.Two.cpp
@@ -9,12 +9,12 @@ using namespace std;
 
 int NormalSeries(int n)
 {
-	if(n == 1)
+	if(n == 2)
 	{
-		return 1;
+		return 2;
 	}
 
-	return n + NormalSeries(n-1);
+	return n + NormalSeries(n-2);
 }
 
 
