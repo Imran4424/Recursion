79d844fce828c2aebd3e3f513dd940582be7161d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-03 23:36:03 +0600

    series summation

---
+++ 1.Series Summation/4.Four.cpp
@@ -0,0 +1,31 @@
+/*
+	4 + 12 + 20 + 28 + .... + n
+
+	find the nth sum of this series using recursion
+*/
+
+#include <iostream>
+using namespace std;
+
+int NormalSeries(int n)
+{
+	if(n == 4)
+	{
+		return 4;
+	}
+
+	return n + NormalSeries(n-8);
+}
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the nth term of the series" << endl;
+
+	int n;
+	cin >> n;
+
+	cout << "The nth summtion of this series is: " << NormalSeries(n) << endl;
+
+	return 0;
+}
