fa88489c1dc31dac8d5bc34d3c7f3d52b24b7ee0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-04 14:51:43 +0600

    series summation

---
+++ 1.Series Summation/14.Forteen.cpp
@@ -0,0 +1,32 @@
+/*
+	1.2.3 + 2.3.4 + 3.4.5 + .... + n
+
+	find the nth sum of this series using recursion
+*/
+
+#include <iostream>
+using namespace std;
+
+int MultiSeries(int n)
+{
+	if(n == 1)
+	{
+		return 1*2*3;
+	}
+
+
+	return n * (n + 1) * (n + 2) + MultiSeries(n-1);
+}
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the nth term of the series" << endl;
+
+	int n;
+	cin >> n;
+
+	cout << "The nth summtion of this series is: " << MultiSeries(n) << endl;
+
+	return 0;
+}
