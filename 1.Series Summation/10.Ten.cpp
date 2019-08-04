32b39621334adb6fe5641f42f9ca927bd4f418d0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-04 12:27:28 +0600

    series summation

--- 1.Series Summation/10.Ten.cpp
+++ 1.Series Summation/10.Ten.cpp
@@ -16,7 +16,7 @@ int MultiSquareSeries(int n)
 
 	int theN = ((n - 1) / 1) + 1; // hence nth-term = a+(n-1)d
 
-	theN--; 
+	theN--; // decrementing one for this series 
 
 	return n*n + MultiSquareSeries(n-1);
 }
