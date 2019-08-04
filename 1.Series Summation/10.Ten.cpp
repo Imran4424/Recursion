93c91ea1627fc8441b43dcf17df0d343313db1d4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-04 12:20:34 +0600

    series summation

---
+++ 1.Series Summation/10.Ten.cpp
@@ -0,0 +1,32 @@
+/*
+	1^2 + 3^2 + 5^2 + .... + n
+
+	find the nth sum of this series using recursion
+*/
+
+#include <iostream>
+using namespace std;
+
+int SquareSeries(int n)
+{
+	if(n == 1)
+	{
+		return 1*1;
+	}
+
+
+	return n*n + SquareSeries(n-2);
+}
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the nth term of the series" << endl;
+
+	int n;
+	cin >> n;
+
+	cout << "The nth summtion of this series is: " << SquareSeries(n) << endl;
+
+	return 0;
+}
