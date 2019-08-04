4f90bf3faaac78a7520d48d2e400efdc2b94dc49
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-04 13:45:55 +0600

    series summation

---
+++ 1.Series Summation/12.Twelve.cpp
@@ -0,0 +1,33 @@
+/*
+	1.2^2 + 2.3^2 + 3.4^2 + .... + n
+
+	find the nth sum of this series using recursion
+*/
+
+#include <iostream>
+using namespace std;
+
+int MultiSquareSeries(int n)
+{
+	if(n == 1)
+	{
+		return 1*2*2;
+	}
+
+	
+
+	return n * (n+1) * (n+1) + MultiSquareSeries(n-1);
+}
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the nth term of the series" << endl;
+
+	int n;
+	cin >> n;
+
+	cout << "The nth summtion of this series is: " << MultiSquareSeries(n) << endl;
+
+	return 0;
+}
