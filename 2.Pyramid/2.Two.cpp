7389544f05f88f4e20607b9edbef91faa1a80b06
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-04 17:47:25 +0600

    pyramid

---
+++ 2.Pyramid/2.Two.cpp
@@ -0,0 +1,49 @@
+/*
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+build the  pyramid using recursion
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void Column(int start, int stop)
+{
+	cout << start << " " ;
+
+	if (start == stop)
+	{
+		cout << endl;
+
+		return;
+	}
+
+	Column(start+1, stop);
+}
+
+void Row(int start, int stop)
+{
+	Column(1, start);
+
+
+	if (start == stop)
+	{
+		return;
+	}
+
+	Row(start+1, stop);
+}
+
+int main(int argc, char const *argv[])
+{
+
+	Row(1, 5); // current pyramid has 5 columns
+
+	return 0;
+}
