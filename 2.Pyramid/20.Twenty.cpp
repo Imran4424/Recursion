1f5244fefabac867554f09cf7d1a33fba0b6c7c4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-05 15:09:13 +0600

    pyramid

---
+++ 2.Pyramid/20.Twenty.cpp
@@ -0,0 +1,50 @@
+/*
+
+A B C D E
+A B C D
+A B C
+A B
+A
+
+build the  pyramid using recursion
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void Column(int start, int stop)
+{
+	cout << (char)(start+64) << " " ;
+
+	if (start == stop)
+	{
+		cout << endl;
+
+		return;
+	}
+
+	Column(start+1, stop); // layer two recursion
+}
+
+void Row(int start, int stop)
+{
+	Column(1, start); // secondary call 
+
+
+	if (start == stop)
+	{
+		return;
+	}
+
+	Row(start+1, stop); // layer one recursion
+}
+
+int main(int argc, char const *argv[])
+{
+	// this is a two layer recursion code
+	// primary call
+	Row(1, 5); // current pyramid has 5 columns
+
+	return 0;
+}
