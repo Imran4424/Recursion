c3f6d9be10c33eb1bbac087a332cb8ec326e122e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-05 12:58:09 +0600

    pyramid

---
+++ 2.Pyramid/13.Thirteen.cpp
@@ -0,0 +1,50 @@
+/*
+
+1 2 3 4 5
+1 2 3 4
+1 2 3 
+1 2 
+1 
+
+build the  pyramid using recursion
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void Column(int start, int stop)
+{
+	cout << start << " " ;
+
+	if (start == stop)
+	{
+		cout << endl;
+
+		return;
+	}
+
+	Column(start+1, stop); // layer two recursion
+}
+
+void Row(int start, int stop)
+{
+	Column(1, stop - start + 1); // secondary call 
+
+
+	if (start == stop)
+	{
+		return;
+	}
+
+	Row(start+1, stop); // layer one recursion
+}
+
+int main(int argc, char const *argv[])
+{
+	// this is a two layer recursion code
+	// primary call
+	Row(1, 5); // current pyramid has 5 columns
+
+	return 0;
+}
