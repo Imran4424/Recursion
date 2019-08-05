e32fefddf1c8aa4087b692dee0679bdf1c6d1d3e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-05 12:26:22 +0600

    pyramid

---
+++ 2.Pyramid/9.Nine.cpp
@@ -0,0 +1,50 @@
+/*
+
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+build the  pyramid using recursion
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void Column(int start, int stop)
+{
+	cout << stop << " " ;
+
+	if (start == stop)
+	{
+		cout << endl;
+
+		return;
+	}
+
+	Column(start+1, stop); // layer two recursion
+}
+
+void Row(int start, int stop)
+{
+	Column(1, start); // secondary call 
+
+
+	if (start == stop)
+	{
+		return;
+	}
+
+	Row(start+1, stop); // layer one recursion
+}
+
+int main(int argc, char const *argv[])
+{
+	// this is a two layer recursion code
+	// primary call
+	Row(1, 5); // current pyramid has 5 columns
+
+	return 0;
+}
