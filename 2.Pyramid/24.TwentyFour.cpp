97a9871b3981a8cf2925d3125d4b92230eeb92bc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-06 08:38:12 +0600

    pyramid

---
+++ 2.Pyramid/24.TwentyFour.cpp
@@ -0,0 +1,72 @@
+/*
+
+        1
+      1 0
+    1 0 1
+  1 0 1 0
+1 0 1 0 1
+
+build the  pyramid using recursion
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void Space(int start, int stop)
+{
+
+	if (start > stop)
+	{
+		return;
+	}
+	
+	cout << "  "; // two spaces, one for digit space, one for space space
+
+	Space(start + 1, stop);
+}
+
+void Column(int start, int stop)
+{
+
+	if (stop % 2 == 0)
+	{
+		cout << "0 ";
+	}
+	else
+	{
+		cout << "1 ";
+	}
+
+	if (start >= stop)
+	{
+		cout << endl;
+
+		return;
+	}
+
+	Column(start+1, stop); // layer two recursion
+}
+
+void Row(int start, int stop)
+{
+	Space(1, stop - start);
+
+	Column(1, start); // secondary call 
+
+	if (start >= stop)
+	{
+		return;
+	}
+
+	Row(start+1, stop); // layer one recursion
+}
+
+int main(int argc, char const *argv[])
+{
+	// this is a two layer recursion code
+	// primary call
+	Row(1, 5); // current pyramid has 5 columns
+
+	return 0;
+}
