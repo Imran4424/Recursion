492166a187281c3d0a28464c7d65d158e7baaf64
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-06 17:28:31 +0600

    pyramid

--- 3. N Digit Binary.cpp/2.UsingGlobalArray.cpp
+++ 3. N Digit Binary.cpp/2.UsingGlobalArray.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
+int bin[100];
+int bits;
 
 
 
 int main(int argc, char const *argv[])
 {
-	
+	cout << "enter the number of bits" << endl;
+	cin >> bits;
+
 	return 0;
 }
