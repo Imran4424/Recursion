07f7e37cbdc4172e3b117780af13810a0f598d58
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-22 16:00:39 +0600

    graph

--- 5.Power Set/2.PowerSetSum.cpp
+++ 5.Power Set/2.PowerSetSum.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int size, bits[100], members[100];
 
+bool firstTimeCall = true;
+
 void DisplaySetSum()
 {
 	int sum = 0;
@@ -40,7 +42,7 @@ int main(int argc, char const *argv[])
 
 	cin >> size;
 
-	cout << enter the members << endl;
+	cout << "enter the members" << endl;
 
 	for (int i = 0; i < size; ++i)
 	{
