4a1e741ad81b694bb5e94b256bdec560e02b373d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-22 11:50:25 +0600

    graph

--- 4.Set/1.PrintSet.cpp
+++ 4.Set/1.PrintSet.cpp
@@ -7,12 +7,14 @@ int main(int argc, char const *argv[])
 {
 	cout << "How many members" << endl;
 
-	cin >> members;
+	cin >> n;
 
-	for (int i = 0; i < members; ++i)
+	for (int i = 0; i < n; ++i)
 	{
 		cin >> members[i];
 	}
+
+	DisplaySet();
 	
 	return 0;
 }
