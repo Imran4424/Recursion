bdab46f735d6649d06376594e5bdb182694887bd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-22 11:57:03 +0600

    graph

--- 4.Set/2.PrintSetBrace.cpp
+++ 4.Set/2.PrintSetBrace.cpp
@@ -5,6 +5,18 @@ int size, members[100];
 
 int main(int argc, char const *argv[])
 {
+	cout << "How many members" << endl;
+
+	cin >> size;
+
+	cout << "enter the members" << endl;
+
+	for (int i = 0; i < size; ++i)
+	{
+		cin >> members[i];
+	}
+
+	DisplaySet();
 	
 	return 0;
 }
