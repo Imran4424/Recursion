9602afb6c9a8759f0850f83f69a413e8f3607317
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-22 11:46:39 +0600

    graph

--- 4.Set/1.PrintSet.cpp
+++ 4.Set/1.PrintSet.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int n, members[100];
 
 int main(int argc, char const *argv[])
 {
-	int 
+	cout << "How many members" << endl;
+
+	cin >> members;
+
+	for (int i = 0; i < members; ++i)
+	{
+		cin >> members[i];
+	}
+	
 	return 0;
 }
