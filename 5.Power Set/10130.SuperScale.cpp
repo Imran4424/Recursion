c48134ed7e934277feb2ddab8ab27ecdbe4c993b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-25 08:44:09 +0600

    dynamic programming

--- 5.Power Set/10130.SuperScale.cpp
+++ 5.Power Set/10130.SuperScale.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+int number, people;
+int *price, *weight, *canCarry;
+
+void ReadCase()
+{
+
+}
+
 int main(int argc, char const *argv[])
 {
-	
+	int test;
+	scanf("%d", &test);
+
+	while(test--)
+	{
+		ReadCase();
+	}
 	return 0;
 }
