db9f533eed7f509bfc5462967cb29d0a8e0761ff
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-28 21:00:11 +0600

    dynamic programming

---
+++ 6.01KnapSack/562.DividingCoinsTD14.cpp
@@ -0,0 +1,116 @@
+/*
+	this is a UVA problem
+
+	562 - Dividing Coins
+
+	this solution is accepted
+
+	here we used dynamic programming top down approach
+
+	Runtime: 0.070s
+*/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+const int  mSize = 101;
+const int sizeSum = 50001;
+
+int dp[mSize][sizeSum];
+
+int size, bits[mSize], coin[mSize], setSum[mSize], total;
+
+void Init()
+{
+	for (int i = 0; i <= size; ++i)
+	{
+		for (int j = 0; j <= total; ++j)
+		{
+			dp[i][j] = -1;
+		}
+	}
+}
+
+int abs(int x)
+{
+	if (x < 0)
+	{
+		return -x;
+	}
+
+	return x;
+}
+
+
+int  Binary(int i, int sum)
+{
+	int left, right; // both holding left and right sub tree values
+
+	if (dp[i][sum] >= 0)
+	{
+		return dp[i][sum];
+	}
+
+	if (i == size)
+	{
+		int sum2 = total - sum;
+
+	
+		return dp[i][sum] = abs(sum - sum2);
+	}
+
+	bits[i] = 0;
+	left = Binary(i+1, sum);
+
+	if (!left)
+	{
+		return 0;
+	}
+
+	bits[i] = 1;
+	right = Binary(i+1, sum + coin[i]);
+
+		
+
+	if (left < right)
+	{
+		return dp[i][sum] = left;
+	}
+	else
+	{
+		return dp[i][sum] = right;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int test;
+	// cin >> test;
+	scanf("%d", &test);
+
+
+	while(test--)
+	{
+		total = 0; // clearing values for every test case
+
+		// cin >> size;
+		scanf("%d", &size);
+
+		for (int i = 0; i < size; ++i)
+		{
+			// cin >> coin[i];
+			scanf("%d", &coin[i]);
+
+			total += coin[i];
+		}
+
+		Init();
+
+		int minimum = Binary(0, 0);
+
+		cout << minimum << endl;
+	}
+	
+	return 0;
+}
