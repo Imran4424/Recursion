c217e4f25c08976f9a9901caaa1db093069b2a41
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-08 11:27:50 +0600

    permutation tree

--- 8.Permutation Tree/2.PermutationInit.cpp
+++ 8.Permutation Tree/2.PermutationInit.cpp
@@ -4,9 +4,24 @@ using namespace std;
 int bin[100];
 int bits;
 
+void Display()
+{
+	for (int i = 0; i < bits; ++i)
+	{
+		cout << bin[i] ;
+	}
+
+	cout << endl;
+}
+
 void Permutation(int i)
 {
+	if (i == bits)
+	{
+		Display();
 
+		return;
+	}
 }
 
 int main(int argc, char const *argv[])
