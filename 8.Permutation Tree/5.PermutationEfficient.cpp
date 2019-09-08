214bb08d6de0d773efd8b5dce45ab3fd45b44516
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-08 12:08:35 +0600

    permutation tree

---
+++ 8.Permutation Tree/5.PermutationEfficient.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int bin[100];
+int visited[100];
+int bits;
+
+
+void InitVisited()
+{
+	for (int i = 0; i < 100; ++i)
+	{
+		visited[i] = 0;
+	}
+}
+
+void Display()
+{
+	for (int i = 0; i < bits; ++i)
+	{
+		cout << bin[i] ;
+	}
+
+	cout << endl;
+}
+
+void Permutation(int i)
+{
+	if (i == bits)
+	{
+		Display();
+
+		return;
+	}
+
+	for (int j = 0; j < bits; ++j) // this is building the initialization for  
+	{                             //  permutation block, but it has duplicates entries
+		                     //   which is not allowed in permutation
+		bin[i] = j;
+
+		Permutation(i+1);
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the number of bits" << endl;
+	cin >> bits;
+
+	cout << endl ;
+
+	Permutation(0);
+
+	return 0;
+}
