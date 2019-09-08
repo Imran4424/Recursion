a10157ba02ac2b3821255e754e5cc3c9fcb18911
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-08 12:03:14 +0600

    permutation tree

---
+++ 8.Permutation Tree/4.Permutation.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int bin[100];
+int bits;
+
+void Display()
+{
+	for (int i = 0; i < bits; ++i)
+	{
+		cout << bin[i] ;
+	}
+
+	cout << endl;
+}
+
+bool IsUnique(int x, int y)
+{
+	for (int i = 0; i < x; ++i)
+	{
+		if (y == a[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void Permutation(int i)
+{
+	if (i == bits)
+	{
+		Display();
+
+		return;
+	}
+
+	for (int j = 0; j < bits; ++j) // this is building the initialization for  
+	{                             //  permutation block, but it has duplicates entries
+		                     //   which is not allowed in permutation
+		bin[i] = j;
+
+		Permutation(i+1);
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the number of bits" << endl;
+	cin >> bits;
+
+	cout << endl ;
+
+	Permutation(0);
+
+	return 0;
+}
