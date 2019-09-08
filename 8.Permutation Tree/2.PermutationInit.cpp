5e0fee425935a91dd94f55c3b29ae2718a23aa64
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-08 11:25:19 +0600

    permutation tree

--- 8.Permutation Tree/2.PermutationInit.cpp
+++ 8.Permutation Tree/2.PermutationInit.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int bin[100];
+int bits;
+
 int main(int argc, char const *argv[])
 {
+	cout << "enter the number of bits" << endl;
+	cin >> bits;
+
+	cout << endl ;
+
+	Binary(0);
 	
 	return 0;
 }
